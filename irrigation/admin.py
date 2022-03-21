d4028796e383e0ce6b28edc259f01bb3c893a5d3
Fausto Márquez <fausto_7m@hotmail.com>
2022-03-21 00:37:31 -0300

    Schedule admin

--- irrigation/admin.py
+++ irrigation/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Schedule)
+
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ("name", "field", "property", "display_weekdays", "is_active")
+    search_fields = ["name", "field", "field", "field__property__name"]
+
+    def property(self, obj):
+        return obj.field.property
--- irrigation/models.py
+++ irrigation/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 
 class Schedule(models.Model):
+    name = models.CharField(max_length=150, null=True, blank=True)
+    is_active = models.BooleanField(default=False)
     field = models.ForeignKey("plants.Field", on_delete=models.CASCADE)
 
     # these three fields are used to define the field's irrigation schedule
@@ -12,5 +14,28 @@ class Schedule(models.Model):
     hours_start = models.JSONField(default=[[], [], [], [], [], [], []])
     durations = models.JSONField(default=[[0], [0], [0], [0], [0], [0], [0]])
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["field", "is_active"], name="unique_field_active"
+            ),
+            models.UniqueConstraint(fields=["field", "name"], name="unique_field_name"),
+        ]
+
+    def display_weekdays(self):
+        def iconize(enabled):
+            if enabled:
+                return "✅"
+            else:
+                return "❌"
+
+        return f"Monday: {iconize(self.weekdays[0])} |\
+            Tuesday: {iconize(self.weekdays[1])} |\
+            Wednesday: {iconize(self.weekdays[2])} |\
+            Thursday: {iconize(self.weekdays[3])} |\
+            Friday: {iconize(self.weekdays[4])} |\
+            Saturday: {iconize(self.weekdays[5])} |\
+            Sunday: {iconize(self.weekdays[6])}"
+
     def __str__(self):
-        return str(self.weekdays)
+        return self.name
