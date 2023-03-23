f4775c23c08e1f246d3528ff886e24926f9b95fc
Fausto Márquez <fausto_7m@hotmail.com>
2023-03-23 19:23:38 -0300

    Changed DB credentials

--- irrigator/settings.py
+++ irrigator/settings.py
@@ -82,14 +82,13 @@ WSGI_APPLICATION = "irrigator.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_NAME"),
+        "NAME": os.environ.get("POSTGRES_DB"),
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
         "HOST": "db",
         "PORT": 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
